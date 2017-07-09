0086f934984586955722215958744ed1af7ab0ee
alexandercch <alexander.cc.h@gmail.com>
2017-07-08 19:44:45 -0500

    part of step 7 tutorial

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from snippets.views import SnippetViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 router = DefaultRouter()
 router.register(r'snippets', SnippetViewSet)
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^schema/$', schema_view),
+    url(r'^/api/', include(router.urls)),
 ]
-
-#urlpatterns = format_suffix_patterns(urlpatterns)
